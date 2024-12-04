091dee5c6305cb0718e63ddf99b7c89808acf9fb
sanaparvee <127944750+sanaparvee@users.noreply.github.com>
2024-12-04 20:42:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "dummy-udp-poc-bucket"
+
+  tags = {
+    Environment = "Development"
+    Project     = "Terraform"
+    Owner       = "TeamA"
+  }
+}
